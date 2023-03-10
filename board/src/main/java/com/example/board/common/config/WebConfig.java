65a017e85a0bafbc98cf54f421c38d5294d31181
rlawhddbs <savagestrikekr@gmail.com>
2023-03-10 23:56:32 +0900

    :recycle: :: Update WebConfig For use to AuthInterceptor

--- board/src/main/java/com/example/board/common/config/WebConfig.java
+++ board/src/main/java/com/example/board/common/config/WebConfig.java
@@ -1,8 +1,19 @@
 package com.example.board.common.config;
 
+import com.example.board.common.interceptor.AuthInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(authInterceptor);
+    }
+
 }
