7ac0bcedd96134935e2242550500bf15a4a9b8fd
rlawhddbs <savagestrikekr@gmail.com>
2023-03-22 19:08:12 +0900

    :tada: :: SMTP Practice Start!

---
+++ SMTP/src/main/java/com/example/SMTP/SmtpApplication.java
@@ -0,0 +1,13 @@
+package com.example.SMTP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmtpApplication.class, args);
+	}
+
+}
